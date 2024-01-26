eeb2ef03beee84bdf9f59e021277f53677d1c4df
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:30:11 +0700

    Add files via upload

---
+++ b2.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+echo "Nhập số nguyên dương a: ";
+$a = trim(fgets(STDIN));
+
+echo "Nhập số nguyên dương b: ";
+$b = trim(fgets(STDIN));
+
+if ($b != 0) {
+    $remainder = $a % $b;
+    echo "Kết quả chia lấy phần dư của $a cho $b là: $remainder";
+} else {
+    echo "Lỗi: b phải khác 0 để thực hiện phép chia.";
+
+}
+echo "\n";
+
+?>
