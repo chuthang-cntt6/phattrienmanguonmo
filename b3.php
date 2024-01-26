cc7d3d320cae87a49ea6763cc7ef8ebd0dc901aa
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:46:58 +0700

    Add files via upload

---
+++ b3.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+echo "Nhập nhiệt độ Fahrenheit: ";
+$fahrenheit = trim(fgets(STDIN));
+
+$celsius = (5 / 9) * ($fahrenheit - 32);
+
+echo "Nhiệt độ Celsius tương đương là: $celsius";
+echo "\n";
+?>
