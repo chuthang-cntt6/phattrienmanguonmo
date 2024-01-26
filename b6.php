f07f2a21e8908137b67a5bdaee010dd9f623a1dd
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:48:40 +0700

    Add files via upload

---
+++ b6.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+echo "Nhập số nguyên dương N có 2 chữ số: ";
+$number = trim(fgets(STDIN));
+
+if ($number >= 10 && $number <= 99) {
+    $digitSum = floor($number / 10) + $number % 10;
+    echo "Tổng các chữ số của $number là: $digitSum";
+} else {
+    echo "Lỗi: N phải là số nguyên dương có 2 chữ số.";
+}
+echo "\n";
+?>
