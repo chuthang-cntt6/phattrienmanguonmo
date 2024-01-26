24f3c9e0e1bbf0ee0c5bda45d155a147c635e1ce
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:48:55 +0700

    Add files via upload

---
+++ b7.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+echo "Nhập thời gian tính bằng giây: ";
+$totalSeconds = trim(fgets(STDIN));
+
+$hours = floor($totalSeconds / 3600);
+$minutes = floor(($totalSeconds % 3600) / 60);
+$seconds = $totalSeconds % 60;
+
+echo "Thời gian là: $hours giờ, $minutes phút, $seconds giây";
+echo "\n";
+?>
