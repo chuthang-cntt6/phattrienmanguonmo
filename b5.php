c396b8ba0f75884f377398db5347e7e64cf9bac3
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:48:29 +0700

    Add files via upload

---
+++ b5.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+echo "Nhập số nguyên a: ";
+$a = trim(fgets(STDIN));
+
+echo "Nhập số nguyên b: ";
+$b = trim(fgets(STDIN));
+
+echo "Nhập số nguyên c: ";
+$c = trim(fgets(STDIN));
+
+$maxValue = max($a, $b, $c);
+echo "Giá trị lớn nhất là: $maxValue";
+echo "\n";
+?>
