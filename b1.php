69ade2cbb366426524e041586487393c561b9447
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:29:28 +0700

    Add files via upload

---
+++ b1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Nhập vào số nguyên n: ";
+$n = trim(fgets(STDIN));
+
+echo "\n";
+
+$absoluteValue = abs($n);
+
+echo "Giá trị tuyệt đối của $n là: $absoluteValue";
+echo "\n";
+?>
+
