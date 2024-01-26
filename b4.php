3bed52ae6548d4ab6d8be5ea32769d12a5b85827
chuthang-cntt6 <126683626+chuthang-cntt6@users.noreply.github.com>
2024-01-26 19:48:14 +0700

    Add files via upload

---
+++ b4.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+echo "Nhập giá trị của a: ";
+$a = trim(fgets(STDIN));
+
+echo "Nhập giá trị của b: ";
+$b = trim(fgets(STDIN));
+
+if ($a != 0) {
+    $x = -$b / $a;
+    echo "Nghiệm của phương trình $a*x + $b = 0 là x = $x";
+} else {
+    echo "Lỗi: a phải khác 0 để giải phương trình.";
+}
+echo "\n";
+?>
